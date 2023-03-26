c56127eafc68c2ad0c2cfdcddafac8f98f16e023
vigneswaran shanmugam <vigneswaranauto@gmail.com>
2023-03-26 19:28:25 +0530

    Update provider.tf

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -2,11 +2,9 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.16"
     }
   }
 
-  required_version = ">= 1.2.0"
   backend "s3" {
     # Replace this with your bucket name!
     bucket         = "sample-application-terraform"
